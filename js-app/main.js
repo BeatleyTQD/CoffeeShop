913ddbb16b62ea8d5d3c58ac1ae1952854d6c0e8
Brandon Wheatley <brandon.a.wheatley@gmail.com>
2020-09-30 10:54:04 -0500

    finished bullet 2 - rendering beans to the dom

--- js-app/main.js
+++ js-app/main.js
@@ -1,13 +1,52 @@
 const url = "https://localhost:5001/api/beanvariety/";
+const url2 = "https://localhost:5001/api/coffee/";
 
 const button = document.querySelector("#run-button");
 button.addEventListener("click", () => {
-    getAllBeanVarieties()
-        .then(beanVarieties => {
-            console.log(beanVarieties);
+    // getAllBeanVarieties()
+    //     .then(beanVarieties => {
+    //         console.log(beanVarieties);
+    //     })
+    // getAllBeanVarieties()
+    //     .then(beanVarieties => beanVarieties.forEach(bean =>
+    //         beanConverter(bean)));
+    showBeans();
+
+
+    getAllCoffees()
+        .then(coffees => {
+            console.log(coffees);
         })
 });
 
 function getAllBeanVarieties() {
     return fetch(url).then(resp => resp.json());
 }
+
+function getAllCoffees() {
+    return fetch(url2).then(resp => resp.json());
+}
+
+function beanConverter(beanObj) {
+    const beanHTML =
+        `<section class="beans">
+        <div>
+             <h3>${beanObj.name}</h3>
+             <p>Region: ${beanObj.region}</p>
+             <p>Notes: ${beanObj.notes}</p>
+             </div>
+    </section>`
+    return beanHTML
+};
+
+function showBeans() {
+    getAllBeanVarieties()
+        .then(beanVarieties => {
+            for (let bean of beanVarieties) {
+                let html = beanConverter(bean);
+                document.querySelector(".beans").innerHTML += html;
+            }
+        })
+}
+
+
